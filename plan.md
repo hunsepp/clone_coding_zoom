ca94fbb090ed387e5878934de02cf62e28b7fa6f
hunsepp <64964615+hunsepp@users.noreply.github.com>
2020-08-08 00:45:18 +0900

    first-commit

---
+++ plan.md
@@ -0,0 +1,11 @@
+# Plan Of Action
+
+-Initialize our NodeJS Project DONE
+-Initialize our first view DONE
+-Create a room id DONE
+-Add the ability to view our own Video
+-Add ability to allow otehrs to stream their video
+-Add styling
+-Add the ablility to create messages
+-Add Mute button
+-Add Stop Video button.
---
+++ server.js
@@ -0,0 +1,23 @@
+//this is the way how we use express library
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+
+//this is library
+const { v4: uuidv4} = require('uuid');
+app.set('view engine','ejs');
+
+//read public folder which is secure
+app.use(express.static('public'));
+
+//`/${} is called string literal
+app.get('/',(req,res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room',(req, res)=>{
+    res.render('room' , {roomId:req.params.room})
+});
+
+
+server.listen(3030);
